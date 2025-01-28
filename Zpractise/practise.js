4303fa2cbb9fa4a01490b39c6ee5014545b36f0a
unknown <ska464@gmail.com>
2025-01-28 00:50:52 -0500

    adding some code

---
+++ Zpractise/practise.js
@@ -0,0 +1,18 @@
+// Write a function to find the longest string in the array.
+
+function findLongestString(arr){
+
+    var lgth = 0;
+
+    var longest = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        lgth.length = arr[i];
+        
+    }
+}
+
+
+console.log(findLongestString(["I", "am", "learning", "JavaScript"])); // "JavaScript"
+console.log(findLongestString(["one", "two", "three"])); // "three"
+console.log(findLongestString(["1", "22", "333"])); // "333"
--- week-3-js-algorithms/task9.js
+++ week-3-js-algorithms/task9.js
@@ -1,19 +1,14 @@
 // 9 - Older Than Me: Create a function that outputs a string specifying if the person in the example below is older or younger than you."
 
-
 // Your code here
-var myage = 35
-function compareAge(name, age){
-    if (age > myage){
-        console.log(`${name} is oler than me`)
-    } else {
-        console.log(`${name} is younger than me `)
-    }
+var myage = 35;
+function compareAge(name, age) {
+  if (age > myage) {
+    console.log(`${name} is oler than me`);
+  } else {
+    console.log(`${name} is younger than me `);
+  }
 }
-
-
-
-
-compareAge("Joel", 36) // ➞ "Joel is older than me."
-compareAge("Samuel", 24) // ➞ "Samuel is younger than me."
-compareAge("Lily", 28) // ➞ "Lily is oder than me."
+compareAge("Joel", 36); // ➞ "Joel is older than me."
+compareAge("Samuel", 24); // ➞ "Samuel is younger than me."
+compareAge("Lily", 28); // ➞ "Lily is oder than me."
